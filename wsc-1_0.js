6f02b80fb46cfdad39afa5b24ad46bbef51fd4bc
nicklink483 <nicklink483@gmail.com>
2013-08-19 03:50:24 -0500

    Create wsc-1_0.js
    File Creation version 1.0

---
+++ wsc-1_0.js
@@ -0,0 +1,71 @@
+/* Any JavaScript here will be loaded for all users on every page load. */
+
+jQuery.fn.doesExist = function(){
+        return jQuery(this).length > 0;
+};
+
+var article_border_color = $("#WikiaFooter section").css("border-color");
+var button_background_color = $("#WikiaPageBackground").css("background-color");
+var button_border_color = article_border_color;
+
+/*
+#WikiaPageHeader {
+  border-bottom: 0;
+  margin-bottom: 0;
+}
+ 
+#WikiaArticle {
+  border-top: 1px solid rgb(204, 204, 204);
+  padding-top: 10px;
+}
+*/
+$("#WikiaArticle").css("border-color", article_border_color).css("border-top", "1px solid").css("padding-top", "10px");
+$("#WikiaPageHeader").css("border-bottom", "0").css("margin-bottom", "0");
+
+// check if div#WikiaRail exists
+
+var sidebar_exists = $("#WikiaRail").doesExist();
+
+var orig_height = parseInt($("#WikiaRail").height(), 10).toString() + "px";
+var new_height = "30px";
+var orig_width = parseInt($("#WikiaArticle").width(), 10).toString() + "px";
+var new_width = parseInt($("#WikiHeader").width(), 10).toString() + "px";
+
+if (sidebar_exists) {
+  $("#WikiaSearch").after('<div id="WikiaSidebarCollapseButton"><div  id="WikiaSidebarCollapseButtonChild"></div></div>');
+  $("#WikiaSidebarCollapseButton").css({
+    position : 'absolute',
+    height : '0px'
+  });
+  $("#WikiaSidebarCollapseButtonChild").css({
+    position : 'relative',
+    left : '340px',
+    top : '-37px',
+    height : '25px',
+    width : '30px',
+    'background-color' : button_background_color,
+    'border-left' : '1px solid' + button_border_color
+  });
+}
+
+var collapsed = false;
+
+function expand() {
+  $("#WikiaRail").css("height", orig_height).css("overflow", "visible");
+  $("#WikiaArticle").css("width", orig_width);
+  collapsed = false;
+}
+
+function collapse() {
+  $("#WikiaRail").css("height", new_height).css("overflow", "hidden");
+  $("#WikiaArticle").css("width", new_width);
+  collapsed = true;
+}
+
+$("#WikiaSidebarCollapseButton").click(function () {
+  if (collapsed) {
+    expand();
+  } else {
+    collapse();
+  }
+});
